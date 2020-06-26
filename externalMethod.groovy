ee4bcd4bc266eb3e2794f17721fd5f69f3d152f1
Baskaran <saravanan.baskaran@capgemini.com>
2020-06-26 09:08:43 +0530

    fixing the error

--- externalMethod.groovy
+++ externalMethod.groovy
@@ -3,9 +3,9 @@ def lookAtThis(String whoAreYou) {
     echo "Look at this, ${whoAreYou} loaded this"
 
 }
+def lookAtMe(String whoAreYou) {
+
+    echo "Look at me, ${whoAreYou} loaded this"
+
+}
 return this
-//def lookAtMe(String whoAreYou) {
-
-//    echo "Look at me, ${whoAreYou} loaded this"
-
-//}
