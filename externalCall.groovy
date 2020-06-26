b43df04cd96f8e0511e4d2ce0f3d90fcb2fae6ce
Baskaran <saravanan.baskaran@capgemini.com>
2020-06-26 09:06:29 +0530

    fixing the error

--- externalCall.groovy
+++ externalCall.groovy
@@ -3,3 +3,4 @@ def call(String whoAreYou) {
    echo "Now we are being called magically by ${whoAreYou}"
 
 }
+return this
--- externalMethod.groovy
+++ externalMethod.groovy
@@ -3,6 +3,7 @@ def lookAtThis(String whoAreYou) {
     echo "Look at this, ${whoAreYou} loaded this"
 
 }
+return this
 //def lookAtMe(String whoAreYou) {
 
 //    echo "Look at me, ${whoAreYou} loaded this"
